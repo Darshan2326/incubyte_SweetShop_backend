cbed31c317f29214235efa46572e20da4db9edd9
Darshan2326 <dramani073@gmail.com>
2025-12-14 13:30:03 +0530

    uat permisson

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://uat-incubyte-sweet-shop-frontend-mxw8wbsz3.vercel.app",
+        "https://uat-incubyte-sweet-shop-frontend-mc9ly2fta.vercel.app/",
         # Add your production domain when ready
         "https://your-production-domain.vercel.app"
     ],
